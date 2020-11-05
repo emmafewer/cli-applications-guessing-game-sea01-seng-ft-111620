377aa240ea27a655db663fe575db07aec20cfb39
Emma Fewer <emmafewer20@gmail.com>
2020-11-05 18:52:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 
 #Generates a random number and prompts the user to guess the number and captures their input. The guessed number will be compared to the random number and will output a message informing whether the numbers match or not or the user has exited the game.
 def run_guessing_game
-  random_number = rand(1..5)
+  random_number = rand(1..6)
   input = gets.chomp
   case input
   when random_number
